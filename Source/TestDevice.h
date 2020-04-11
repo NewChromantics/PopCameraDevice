494d9e263fcf5c736bcca542366c50e6a813ff06
Graham Reeves <graham@grahamreeves.com>
2020-04-11 21:59:01 +0100

    Test device now outputs more than one frame

--- Source/TestDevice.h
+++ Source/TestDevice.h
@@ -9,6 +9,7 @@ public:
 	static inline const char* DeviceName = "Test";
 public:
 	TestDevice(const std::string& Format);
+	~TestDevice();
 
 	static void											EnumDeviceNames(std::function<void(const std::string&)> Enum);
 
@@ -17,4 +18,8 @@ public:
 
 	SoyPixelsMeta	mMeta = SoyPixelsMeta(16, 16, SoyPixelsFormat::Greyscale);
 	size_t			mFrameRate = 30;
+	size_t			mFrameNumber = 0;
+	bool			mRunning = true;
+	
+	std::shared_ptr<SoyThread>	mThread;
 };
